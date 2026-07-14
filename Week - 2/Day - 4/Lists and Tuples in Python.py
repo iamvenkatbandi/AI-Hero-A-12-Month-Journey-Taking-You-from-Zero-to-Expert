72f703a534382a98139fe9ddd514e687bde69427
Bandi Venkat Chowdary <178579004+iamvenkatbandi@users.noreply.github.com>
2026-07-14 09:57:47 +0530

    Uploading Day - 4 Practice Files

---
+++ Week - 2/Day - 4/Lists and Tuples in Python.py
@@ -0,0 +1,23 @@
+# Creating a list of your 5 favorite movies
+movies = ["Bahubali", "RRR", "Maharshi", "Bharat Ane Nenu", "Drushyam"]
+print(type(movies),"\n")
+print(movies,"\n")
+
+# Adding one more movie using append()
+movies.append("Kitakitalu")
+print("After Appending...\n")
+print(movies,"\n")
+
+# Using Slicing to print only first 3 movies
+print("After Slicing...\n")
+print(movies[0:3],"\n")
+
+# Creating Tuples for RGB Colors
+red = (255,0,0)
+blue = (0,255,0)
+green = (0,0,255)
+color = (255,0,128)
+
+print(type(color),"\n")
+
+print(color)
